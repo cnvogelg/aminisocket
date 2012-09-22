1658f68f82a2000c5f255a106a8e78fbe76f2b9a
Christian Vogelgsang <C.Vogelgsang@web.de>
2012-09-22 16:55:41 +0200

    revised virtual parport protocol

--- src/pysockslave/pkt.py
+++ src/pysockslave/pkt.py
@@ -11,24 +11,21 @@ class Pkt:
     def __init__(self, vpar):
         self.vpar = vpar
         # first make sure we have updated values - drain data and status channels
-        while True:
-            (dat, status) = self.vpar.wait_data_or_status(0)
-            if dat == False and status == False:
-                break
+        self.vpar.drain()
     
     def wait_select(self, value, timeout=1):
         """wait for SELECT signal"""
         t = time.time()
         end = t + timeout
         while t < end:
-            s = self.vpar.get_status() & vpar.SEL_MASK
+            s = self.vpar.peek_status() & vpar.SEL_MASK
             if s and value:
                 return True
             elif not s and not value:
                 return True
             
             rem = end - t
-            self.vpar.wait_data_or_status(rem)
+            self.vpar.read(rem)
             t = time.time()
         return False
     
@@ -38,27 +35,29 @@ class Pkt:
         t = time.time()
         end = t + timeout
         while t < end:
-            s = self.vpar.get_status()
+            s = self.vpar.peek_status()
             if expect and ((s & vpar.POUT_MASK) == vpar.POUT_MASK):
                 return True
             elif (not expect) and ((s & vpar.POUT_MASK) == 0):
                 return True
+            
             # check for SELECT -> arbitration loss?
             if (s & vpar.SEL_MASK) != vpar.SEL_MASK:
                 print "LOST SELECT"
                 return False
+                
             rem = end - t
-            self.vpar.wait_data_or_status(rem)
+            self.vpar.read(rem)
             t = time.time()
         return False
     
     def toggle_busy(self):
-        s = self.vpar.read_status()
+        s = self.vpar.get_status()
         if s & vpar.BUSY_MASK:
             n = s & ~vpar.BUSY_MASK
         else:
             n = s | vpar.BUSY_MASK
-        self.vpar.write_status(n)
+        self.vpar.set_status(n)
         #print "toggle %02x -> %02x\n" % (s,n)
             
     def set_next_byte(self, toggle_expect, value, timeout=1):
@@ -66,7 +65,7 @@ class Pkt:
         if not ok:
             print "FAILED: wait for",toggle_expect
             return False
-        self.vpar.write_data(value)
+        self.vpar.set_data(value)
         self.toggle_busy()
         return True
 
@@ -84,11 +83,11 @@ class Pkt:
         """send a packet via vpar port"""
         # set HS_LINE (BUSY)
         s = self.vpar.get_status() | vpar.BUSY_MASK
-        self.vpar.write_status(s)
+        self.vpar.set_status(s)
         # set magic byte
-        self.vpar.write_data(MAGIC)
+        self.vpar.set_data(MAGIC)
         # pulse ACK -> trigger IRQ in server
-        self.vpar.write_status(s | vpar.ACK_MASK)
+        self.vpar.trigger_ack()
         # wait for select
         ok = self.wait_select(True)
         if not ok:
@@ -121,5 +120,9 @@ class Pkt:
             print "ERROR: final toggle"
         # clear HS_LINE (BUSY)
         s = self.vpar.get_status() & ~vpar.BUSY_MASK
-        self.vpar.write_status(s)
+        self.vpar.set_status(s)
+        # wait for select end
+        ok = self.wait_select(False)
+        if not ok:
+            print "ERROR: Waiting for unselect"
         return ok
--- src/pysockslave/vpar.py
+++ src/pysockslave/vpar.py
@@ -9,74 +9,68 @@ SEL_MASK = 4
 ACK_MASK = 8
 
 class VPar:
-    def __init__(self, ctl_name=None, dat_name=None):
-        self.ctl_name = ctl_name
-        self.dat_name = dat_name
-        self.ctl_file = open(self.ctl_name, "r+b", 0)
-        self.dat_file = open(self.dat_name, "r+b", 0)
+    def __init__(self, par_name=None):
+        self.par_name = par_name
+        self.par_file = open(self.par_name, "r+b", 0)
         self.ctl = 0
         self.dat = 0
     
     def close(self):
-        self.ctl_file.close()
-        self.dat_file.close()
+        self.par_file.close()
 
-    def write_data(self, val):
-        """write a byte to the parallel port"""
-        self.dat_file.write(chr(val))
-        self.dat = val
+    def drain(self):
+        """slurp all incoming data"""
+        while self.has_data():
+            self.par_file.read(1)
+            
+    def write(self):
+        """write status and data"""
+        self.par_file.write(chr(self.ctl) + chr(self.dat))
+        print("tx: ctl=%02x dat=%02x" % (self.ctl, self.dat))
 
-    def has_data(self):
+    def has_data(self, timeout=0):
         """poll if new data is available"""
-        ready = select.select([self.dat_file],[],[],0)[0]
+        ready = select.select([self.par_file],[],[],timeout)[0]
         return len(ready)
 
-    def read_data(self):
-        """read byte from parallel port"""
+    def read(self, timeout=0):
+        """read status and data"""
         # poll port - if something is here read it first
-        if self.has_data():
-            self.dat = ord(self.dat_file.read(1))
-        return self.dat
+        if self.has_data(timeout):
+            d = self.par_file.read(2)
+            self.ctl = ord(d[0])
+            self.dat = ord(d[1])
+            print("rx: ctl=%02x dat=%02x" % (self.ctl, self.dat))
+            
+    def trigger_ack(self):
+        self.par_file.write(chr(self.ctl | ACK_MASK) + chr(self.dat))
+        print("tx: ctl=%02x dat=%02x" % (self.ctl, self.dat))        
 
-    def write_status(self, val):
-        """write a new status flag value"""
-        self.ctl_file.write(chr(val))
-        self.ctl = val
-    
     def set_status(self, val):
-        self.write_status(self.ctl | val)
+        self.ctl = val
+        self.write()
         
-    def clr_status(self, val):
-        self.write_status(self.ctl & ~val)
-    
-    def has_status(self):
-        """poll if new status is available"""
-        ready = select.select([self.ctl_file],[],[],0)[0]
-        return len(ready)>0
-
-    def read_status(self):
-        """read status flag values"""
-        # poll port
-        if self.has_status():
-            self.ctl = ord(self.ctl_file.read(1))
+    def set_data(self, val):
+        self.dat = val
+        self.write()
+        
+    def get_status(self, timeout=0):
+        self.read(timeout)
         return self.ctl
-        
-    def wait_data_or_status(self, timeout=0):
-        """block until status or data changes. read updated value.
-           return (data_available, status_available) bool flag """
-        ready = select.select([self.ctl_file, self.dat_file],[],[],timeout)[0]
-        got_data = False
-        got_status = False
-        if self.dat_file in ready:
-            self.dat = ord(self.dat_file.read(1))
-            got_data = True
-        if self.ctl_file in ready:
-            self.ctl = ord(self.ctl_file.read(1))
-            got_status = True
-        return (got_data, got_status)
-
-    def get_data(self):
+    
+    def get_data(self, timeout=0):
+        self.read(timeout)
         return self.dat
-    
-    def get_status(self):
+        
+    def set(self, ctl, dat):
+        self.ctl = ctl
+        self.dat = dat
+        self.write()
+        
+    def get(self, timeout=0):
+        self.read(timeout)
+        return (self.ctl, self.dat)
+        
+    def peek_status(self):
         return self.ctl
+
