b622203d2bc43abae51167277a4442bd6f9d299f
Christian Vogelgsang <C.Vogelgsang@web.de>
2012-10-02 21:01:09 +0200

    added some api commands

--- src/common/msg.h
+++ src/common/msg.h
@@ -9,18 +9,30 @@ struct msg_s {
     struct Message m_msg;
     UWORD          m_command;
     UWORD          m_result;
-    UWORD          m_handle;
     UWORD          m_flags;
+    UWORD          m_extra;
+    ULONG          m_handle;
     APTR           m_data;
     ULONG          m_size;
+    APTR           m_addr_data;
+    ULONG          m_addr_size;
 };
 typedef struct msg_s msg_t;
 
 #define SRV_PORT_NAME           "socksrv"
 
-#define MSG_CMD_HELLO           0
-#define MSG_CMD_CREATE_SOCKET   1
-#define MSG_CMD_CLOSE_SOCKET    2
+#define MSG_CMD_CREATE_SOCKET       0
+#define MSG_CMD_BIND_SOCKET         1
+#define MSG_CMD_LISTEN_SOCKET       2
+#define MSG_CMD_ACCEPT_SOCKET       3
+#define MSG_CMD_CONNECT_SOCKET      4
+#define MSG_CMD_SEND_TO_SOCKET      5
+#define MSG_CMD_SEND_SOCKET         6
+#define MSG_CMD_RECV_FROM_SOCKET    7
+#define MSG_CMD_RECV_SOCKET         8
+#define MSG_CMD_SHUTDOWN_SOCKET     9
+
+#define MSG_CMD_CLOSE_SOCKET        10
 
 #define MSG_RESULT_OK           0
 #define MSG_RESULT_NO_SERVER    1
--- src/lib/bsdsocket.c
+++ src/lib/bsdsocket.c
@@ -13,16 +13,52 @@ static LONG local_errno = 0;
 static LONG *errno_ptr = &local_errno;
 
 /* errno values */
-#define EACCESS     2
+#define EACCESS       2
+#define ENOMEM        12
+#define EAFNOSUPPORT  47
 #define SET_ERRNO(x) *errno_ptr = -x
 
+static void set_msg_errno(msg_t *msg)
+{
+    D(bug("set errno: %d\n", msg->m_result));
+    /* TODO: determine value from result */
+    SET_ERRNO(ENOMEM);
+}
+
+static int check_msg_errno(msg_t *msg)
+{
+    /* check result */
+    if(msg->m_result != MSG_RESULT_OK) {
+        set_msg_errno(msg);
+        return -1;
+    } else {
+        return 0;            
+    }
+}
+
+/* ----- BSDSocket API ------------------------------------------------------ */
+
+/* socket */
 __asm __saveds int lib_socket(register __d0 int domain, register __d1 int type, register __d2 int protocol )
 {
     int err;
     
     D(bug("socket(%ld,%ld,%ld)\n", domain, type, protocol));
     
-
+    /* do some checks on supported arguments */
+    if(domain != PF_INET)
+    {
+        D(bug("only domain PF_INET supported!\n"));
+        SET_ERRNO(EAFNOSUPPORT);
+        return -1;
+    }
+    if( (type != SOCK_STREAM) && (type != SOCK_DGRAM) ) 
+    {
+        D(bug("only type SOCK_STREAM or SOCK_DGRAM supported!\n"));
+        SET_ERRNO(EAFNOSUPPORT);
+        return -1;
+    }
+    
     /* setup msgio */
     err = msgio_init();
     if(err != 0) {
@@ -31,109 +67,226 @@ __asm __saveds int lib_socket(register __d0 int domain, register __d1 int type, 
         return -1;
     }
 
-    /* prepare message */
+    /* create socket */
     msg->m_command = MSG_CMD_CREATE_SOCKET;
     msgio_xfer_msg(msg);
-    return 0;
+    
+    /* check result */
+    if(msg->m_result != MSG_RESULT_OK) {
+        set_msg_errno(msg);
+        return -1;
+    } else {
+        return (int)msg->m_handle;
+    }
 }
 
+/* bind */
 __asm __saveds int lib_bind(register __d0 int sock, register __a0 struct sockaddr * name, register __d1 int namelen )
 {
     D(bug("bind(%ld,%lx,%ld)\n", sock, name, namelen));
-    return 0;
+    
+    msg->m_command = MSG_CMD_BIND_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_addr_data = (APTR)name;
+    msg->m_addr_size = (ULONG)namelen;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* listen */
 __asm __saveds int lib_listen(register __d0 int sock, register __d1 int backlog )
 {
     D(bug("listen(%ld,%ld)\n", sock, backlog));
-    return 0;
+    
+    msg->m_command = MSG_CMD_LISTEN_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_extra = (UWORD)backlog;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* accept */
 __asm __saveds int lib_accept(register __d0 int sock, register __a0 struct sockaddr * addr, register __a1 int addrlen )
 {
+    int err;
+    
     D(bug("accept(%ld,%lx,%ld)\n", sock, addr, addrlen));
-    return 0;
+    
+    msg->m_command = MSG_CMD_ACCEPT_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_addr_data = (APTR)addr;
+    msg->m_addr_size = (ULONG)addrlen;
+    msgio_xfer_msg(msg);
+    
+    err = check_msg_errno(msg);
+    if(err == 0) {
+        return (int)msg->m_handle; /* TODO: new handle */
+    } else {
+        return -1;
+    }
 }
 
+/* connect */
 __asm __saveds int lib_connect(register __d0 int sock, register __a0 struct sockaddr * name, register __d1 int namelen )
 {
     D(bug("connect(%ld,%lx,%ld)\n", sock, name, namelen));
-    return 0;
+    
+    msg->m_command = MSG_CMD_CONNECT_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_addr_data = (APTR)name;
+    msg->m_addr_size = (ULONG)namelen;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* sendto */
 __asm __saveds int lib_sendto(register __d0 int sock, register __a0 const void * buf, register __d1 int len, register __d2 int flags, register __a1 const struct sockaddr * to, register __d3 int tolen )
 {
     D(bug("sendto(%ld,%lx,%ld,%ld,%lx,%ld)\n", sock, buf, len, flags, to, tolen));
-    return len;
+    
+    msg->m_command = MSG_CMD_SEND_TO_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_data = (APTR)buf;
+    msg->m_size = (ULONG)len;
+    msg->m_flags = (UWORD)flags;
+    msg->m_addr_data = (APTR)to;
+    msg->m_addr_size = (ULONG)tolen;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* send */
 __asm __saveds int lib_send(register __d0 int sock, register __a0 const void * buf, register __d1 int len, register __d2 int flags )
 {
     D(bug("send(%ld,%lx,%ld,%ld)\n", sock, buf, len, flags));
-    return 0;
+    
+    msg->m_command = MSG_CMD_SEND_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_data = (APTR)buf;
+    msg->m_size = (ULONG)len;
+    msg->m_flags = (UWORD)flags;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* recvfrom */
 __asm __saveds int lib_recvfrom(register __d0 int sock, register __a0 void * buf, register __d1 int len, register __d2 int flags, register __a1 struct sockaddr * addr, register __a2 int addrlen )
 {
     D(bug("recvfrom(%ld,%lx,%ld,%ld,%lx,%ld)\n", sock, buf, len, flags, addr, addrlen));
-    return len;
+    
+    msg->m_command = MSG_CMD_RECV_FROM_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_data = (APTR)buf;
+    msg->m_size = (ULONG)len;
+    msg->m_flags = (UWORD)flags;
+    msg->m_addr_data = (APTR)addr;
+    msg->m_addr_size = (ULONG)addrlen;    
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* recv */
 __asm __saveds int lib_recv(register __d0 int sock, register __a0 void * buf, register __d1 int len, register __d2 int flags )
 {
     D(bug("recv(%ld,%lx,%ld,%ld)\n", sock, buf, len, flags));
-    return len;
+    
+    msg->m_command = MSG_CMD_RECV_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_data = (APTR)buf;
+    msg->m_size = (ULONG)len;
+    msg->m_flags = (UWORD)flags;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* shutdown */
 __asm __saveds int lib_shutdown(register __d0 int sock, register __d1 int how )
 {
     D(bug("shutdown(%ld,%ld)\n", sock, how));
-    return 0;
+    
+    msg->m_command = MSG_CMD_SHUTDOWN_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msg->m_extra = (UWORD)how;
+    msgio_xfer_msg(msg);
+    
+    return check_msg_errno(msg);
 }
 
+/* setsockopt */
 __asm __saveds int lib_setsockopt(register __d0 int sock, register __d1 int level, register __d2 int optname, register __a0 void * optval, register __d3 int optlen )
 {
     D(bug("setsockopt(%ld,%ld,%ld,%lx,%ld)\n", sock, level, optname, optval, optlen));
+    
+    /* TODO */
+    
     return 0;
 }
 
+/* getsockopt */
 __asm __saveds int lib_getsockopt(register __d0 int sock, register __d1 int level, register __d2 int optname, register __a0 void * optval, register __a1 int optlen )
 {
     D(bug("getsockopt(%ld,%ld,%ld,%lx,%ld)\n", sock, level, optname, optval, optlen));
+    
+    /* TODO */
+    
     return 0;
 }
 
+/* getsockname */
 __asm __saveds int lib_getsockname(register __d0 int sock, register __a0 struct sockaddr * name, register __a1 int namelen )
 {
     D(bug("getsockname(%ld,%lx,%ld)\n", sock, name, namelen));
     return 0;
 }
 
+/* getpeername */
 __asm __saveds int lib_getpeername(register __d0 int sock, register __a0 struct sockaddr * name, register __a1 int namelen )
 {
     D(bug("getpeername(%ld,%lx,%ld)\n", sock, name, namelen));
     return 0;
 }
 
+/* ioctl */
 __asm __saveds int lib_IoctlSocket(register __d0 int sock, register __d1 unsigned long req, register __a0 char * argp )
 {
     D(bug("IoctlSocket(%ld,%lx,%lx)\n", sock, req, argp));
     return 0;
 }
 
+/* close */
 __asm __saveds int lib_CloseSocket(register __d0 int sock )
 {
+    int err;
+    
     D(bug("CloseSocket(%ld)\n", sock));
+    
+    /* send close command */
+    msg->m_command = MSG_CMD_CLOSE_SOCKET;
+    msg->m_handle = (ULONG)sock;
+    msgio_xfer_msg(msg);
+    err = check_msg_errno(msg);
+    
+    /* shutdown msgio if last socket */
     msgio_shutdown();
-    return 0;
+    
+    return err;
 }
 
+/* select */
 __asm __saveds int lib_WaitSelect(register __d0 int nfds, register __a0 fd_set * read_fds, register __a1 fd_set * write_fds, register __a2 fd_set * except_fds, register __a3 struct timeval * timeout, register __d1 ULONG * signals )
 {
     D(bug("WaitSelect(%ld,%lx,%lx%,p,%lx,%lx)\n", nfds, read_fds, write_fds, except_fds, timeout, signals));
     return 0;
 }
 
+/* set signals */
 __asm __saveds void lib_SetSocketSignals(register __d0 ULONG int_mask, register __d1 ULONG io_mask, register __d2 ULONG urgent_mask )
 {
     D(bug("SetSocketSignals(%ld,%ld,%ld)\n", int_mask, io_mask, urgent_mask));
@@ -148,19 +301,19 @@ __asm __saveds int lib_getdtablesize( void  )
 __asm __saveds LONG lib_ObtainSocket(register __d0 LONG id, register __d1 LONG domain, register __d2 LONG type, register __d3 LONG protocol )
 {
     D(bug("ObtainSocket(%ld,%ld,%ld,%ld)\n", id, domain, type, protocol));
-    return 0;
+    return -1; /* not supported */
 }
 
 __asm __saveds LONG lib_ReleaseSocket(register __d0 LONG sock, register __d1 LONG id )
 {
     D(bug("ReleaseSocket(%ld,%ld)\n", sock, id));
-    return 0;
+    return -1; /* not supported */
 }
 
 __asm __saveds LONG lib_ReleaseCopyOfSocket(register __d0 LONG sock, register __d1 LONG id )
 {
     D(bug("ReleasecCopyOfSocket(%ld,%ld)\n", sock, id));
-    return 0;
+    return -1; /* not supported */
 }
 
 __asm __saveds long lib_Errno( void  )
